f60246391961db8e8221536b8035ca291e5d67f5
Rehman Aziz <rehmanaziz20@yahoo.com>
2020-10-07 23:16:59 +0500

    modules done

--- python_sandbox_starter/modules.py
+++ python_sandbox_starter/modules.py
@@ -1,1 +1,15 @@
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+import datetime
+from datetime import date
+import validator
+import camelcase
+
+my_date=date.today()
+print(my_date)
+myEmail='rehmanaziz20yahoo.com'
+if validator.validate_email(myEmail):
+    print('Valid Email')
+else:
+    print('invalid Email')
+
+print(camelcase.CamelCase().hump('my name is rehman aziz'))
