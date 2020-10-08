d9dfcbcc318a0cdb2514a54891ce4bf7fc3d8ee6
Rehman Aziz <rehmanaziz20@yahoo.com>
2020-10-08 16:23:41 +0500

    files done

--- python_sandbox_starter/files.py
+++ python_sandbox_starter/files.py
@@ -1,1 +1,24 @@
 # Python has functions for creating, reading, updating, and deleting files.
+myFile=open('name.txt','w')
+
+print('Name of File: '+myFile.name)
+print('Mode of Operation: '+myFile.mode)
+
+myFile.write('My name is Rehman Aziz.\n')
+myFile.write('I am 23 years old.')
+
+myFile.close()
+
+myFile=open('name.txt','a')
+
+myFile.write('\nI like python and djando\n')
+
+myFile.close()
+
+myFile=open('name.txt','r')
+
+strContent=myFile.read(100)
+
+print(strContent)
+
+myFile.close()
