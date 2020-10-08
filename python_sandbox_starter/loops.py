b6051acec570e27bbb22ffa73673e44baa64cf30
Rehman Aziz <rehmanaziz20@yahoo.com>
2020-10-08 15:30:04 +0500

    loops done

--- python_sandbox_starter/loops.py
+++ python_sandbox_starter/loops.py
@@ -1,5 +1,17 @@
 # A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
 
+cars=['honda civic','toyota vitz','honda br-v','toyota land cruiser','audi']
 
-
+for car in cars:
+    print(car)
+j=0
+for j in range(0,6,2):
+    print(j) 
 # While loops execute a set of statements as long as a condition is true.
+i=0
+while i<10:
+    print(f'--{i}--')
+    i+=1
+else:
+    print('loop ended')
+
