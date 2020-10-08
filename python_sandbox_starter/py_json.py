728b5aa211ae282a3a3800240f8761227da130c1
Rehman Aziz <rehmanaziz20@yahoo.com>
2020-10-08 16:48:14 +0500

    python json done

--- python_sandbox_starter/py_json.py
+++ python_sandbox_starter/py_json.py
@@ -1,1 +1,20 @@
 # JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+import json
+
+myInfo={
+    'name': 'Rehman Aziz',
+    'age':23,
+    'university': 'G.C University',
+    'semester':7
+}
+
+myInfoStr='{"name":"Sunny","age":20,"dob":"29-jan-1997"}'
+
+print(myInfo)
+
+user=json.dumps(myInfo)
+userJson=json.loads(myInfoStr)
+
+print(user)
+
+print(userJson)
